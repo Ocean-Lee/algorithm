06f26e106bb101a1d6eb4f6059ec79ed6178c489
Lee <>
2019-08-22 12:04:23 +0800

    1.TwoSum
    new a Map and Search Map

--- src/main/java/leetcode/TwoSum.java
+++ src/main/java/leetcode/TwoSum.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个整数数组 nums 和一个目标值 target，
  * 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
@@ -19,17 +22,18 @@ public class TwoSum {
      * O(1)
      * <p>
      * <p>
-     * 问题:如何能降低时间复杂度？
+     * 问题:对于每个元素，试图通过遍历数组其余部分来找对应的目标元素
      *
      * @param nums
      * @param target
      * @return
      */
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSumBrutal(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (j < nums.length && nums[i] + nums[j] == target) {
+                    //int[] result = new int[2];
                     result[0] = i;
                     result[1] = j;
                     return result;
@@ -39,9 +43,35 @@ public class TwoSum {
         return null;
     }
 
+    /**
+     * 将数组的元素保存到Map中，减少查找对应元素的次数
+     * 时间复杂度:O(n)
+     * 空间复杂度:O(n)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSumMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            Integer object = map.get(target - nums[i]);
+            if (object != null && object != i) {
+                int[] result = new int[2];
+                result[0] = i;
+                result[1] = object;
+                return result;
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int[] result = new TwoSum().twoSum(nums, 26);
+        int[] nums = {3, 2, 4};
+        int[] result = new TwoSum().twoSumMap(nums, 6);
         for (int i : result) {
             System.out.println(i);
         }
